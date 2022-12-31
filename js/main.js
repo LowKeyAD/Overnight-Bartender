3f143dc1007f38f8741fadd18cf8fab4b23ad46f
Anthony Douglas Jr <101023695+LowKeyAD@users.noreply.github.com>
2022-12-30 21:43:23 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+
+    let drink = document.querySelector('input').value
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.drinks)
+      document.querySelector('h3').innerText = data.drinks[0].strDrink
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      document.querySelector('h4').innerText = data.drinks[0].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+}
+
+
+//create carousel of drinks
+//try without template literal
+//get nasa picture of the day api
+
+
+//Without a template literal******************
+
+/*
+
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+
+    let drink = document.querySelector('input').value
+
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+drink)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.drinks)
+      document.querySelector('h2').innerText = data.drinks[0].strDrink
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      document.querySelector('h3').innerText = data.drinks[0].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+}
+
+
+*/
